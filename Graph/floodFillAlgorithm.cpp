0ea96d2b4f67aa381ed992749a5af8128cbb6e3b
ansariarifdev <ansariarif.dev@gamil.com>
2025-07-02 00:18:26 +0530

    solved flood fil algorithm problem: both ways

--- Graph/floodFillAlgorithm.cpp
+++ Graph/floodFillAlgorithm.cpp
@@ -2,14 +2,33 @@
 #include <vector>
 using namespace std;
 
-// Function to perform flood fill using DFS
-void dfs(vector<vector<int>>& image, int sr, int sc, int oldColor, int newColor) {
-    // Your implementation goes here
-}
+// Implementation of the solve function 
+void solve(vector<vector<int>> &image, vector<vector<int>> &imageCopy, int sr, int sc, vector<int> &delrow, vector<int> &delcol, int initialColor, int newColor) {
+    imageCopy[sr][sc] = newColor;
 
+    int n = image.size();
+    int m = image[0].size();
+
+    for(int i = 0; i < 4; i++) {
+        int newrow = sr + delrow[i];
+        int newcol = sc + delcol[i];
+
+        if(newrow >= 0 && newrow < n && newcol >= 0 && newcol < m && image[newrow][newcol] == initialColor && imageCopy[newrow][newcol] != newColor) {
+            solve(image, imageCopy, newrow, newcol, delrow, delcol, initialColor, newColor);
+        }
+    }
+}
 // Function to flood fill the image
 vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
     // Your implementation goes here
+    int initialColor = image[sr][sc];
+    vector<vector<int>> imageCopy = image;
+
+    vector<int> delrow = {-1, 0, +1, 0};
+    vector<int> delcol = {0, +1, 0, -1};
+
+    solve(image, imageCopy, sr, sc, delrow, delcol, initialColor, newColor);
+    return imageCopy;
 }
 
 // Helper function to print the image
--- Graph/floodFillAlgorithmusingBfs.cpp
+++ Graph/floodFillAlgorithmusingBfs.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
+
+// Implementation of the solve function 
+void solve(vector<vector<int>> &image, vector<vector<int>> &imageCopy, int sr, int sc, int initialColor, int newColor, vector<int> &delrow, vector<int> &delcol) {
+    imageCopy[sr][sc] = newColor;
+    int n = image.size();
+    int m = image[0].size();
+
+    queue<pair<int, int>> q;
+    q.push({sr, sc});
+
+    while(!q.empty()) {
+        auto [row, col] = q.front();
+        q.pop();
+
+        for(int i = 0; i < 4; i++) {
+            int newrow = row + delrow[i];
+            int newcol = col + delcol[i];
+
+            if(newrow >= 0 && newrow < n && newcol >= 0 && newcol < m && image[newrow][newcol] == initialColor && imageCopy[newrow][newcol] != newColor) {
+                imageCopy[newrow][newcol] = newColor;
+                q.push({newrow, newcol});
+            }
+        }
+    }
+}
 
 // Function to perform flood fill using BFS
 vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-    // Your implementation goes here
+   int initialColor = image[sr][sc];
+
+   // make a copy of the actual(input) data;
+   vector<vector<int>> imageCopy = image;
+
+   vector<int> delrow = {-1, 0, +1, 0};
+   vector<int> delcol = {0, +1, 0, -1};
+
+   solve(image, imageCopy, sr, sc, initialColor, newColor, delrow, delcol);
+
+   return imageCopy;
 }
 
 // Helper function to print the image
